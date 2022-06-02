ea2dd3993d45aeb68efb2e297bd8136c040708a8
TropheusJ <jverrellijr@icloud.com>
2022-06-01 21:55:12 -0400

    always the last-minute refactors...

--- src/main/java/io/github/tropheusj/serialization_hooks/mixin/IngredientMixin.java
+++ src/main/java/io/github/tropheusj/serialization_hooks/mixin/IngredientMixin.java
@@ -5,6 +5,8 @@ import com.google.gson.JsonElement;
 
 import com.google.gson.JsonObject;
 
+import com.google.gson.JsonSyntaxException;
+
 import io.github.tropheusj.serialization_hooks.IngredientDeserializer;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
@@ -12,7 +14,6 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -22,51 +23,20 @@ public abstract class IngredientMixin {
 	private static void serialization_hooks$fromNetwork(FriendlyByteBuf buffer, CallbackInfoReturnable<Ingredient> cir) {
 		ResourceLocation id = buffer.readResourceLocation();
 		if (!id.equals(IngredientDeserializer.NONE)) {
-			IngredientDeserializer serializer = IngredientDeserializer.REGISTRY.get(id);
-			if (serializer == null)
+			IngredientDeserializer deserializer = IngredientDeserializer.REGISTRY.get(id);
+			if (deserializer == null)
 				throw new IllegalStateException("[SerializationHooks] IngredientSerializer with ID not found: " + id);
-			cir.setReturnValue(serializer.fromPacket(buffer));
+			cir.setReturnValue(deserializer.fromPacket(buffer));
 		}
 	}
 
-	@Inject(
-			method = "fromJson",
-			at = @At(
-					value = "INVOKE",
-					target = "Lcom/google/gson/JsonElement;isJsonObject()Z",
-					shift = Shift.BY,
-					by = 2
-			),
-			cancellable = true
-	)
+	@Inject(method = "fromJson", at = @At("HEAD"), cancellable = true)
 	private static void serialization_hooks$fromJsonObject(JsonElement json, CallbackInfoReturnable<Ingredient> cir) {
-		JsonObject obj = json.getAsJsonObject();
-		JsonElement serializerElement = obj.get("serializer");
-		if (serializerElement == null)
-			return;
-		if (!serializerElement.isJsonPrimitive())
-			throw new IllegalStateException("[SerializationHooks] A serializer is declared, but it is not a primitive: " + serializerElement);
-		ResourceLocation id = new ResourceLocation(serializerElement.getAsString());
-		IngredientDeserializer serializer = IngredientDeserializer.REGISTRY.get(id);
-		if (serializer == null)
-			throw new IllegalStateException("[SerializationHooks] IngredientSerializer with ID not found: " + id);
-		cir.setReturnValue(serializer.fromJsonObject(obj));
-	}
-
-	@Inject(
-			method = "fromJson",
-			at = @At(
-					value = "INVOKE",
-					target = "Lcom/google/gson/JsonElement;isJsonArray()Z",
-					shift = Shift.BY,
-					by = 2
-			),
-			cancellable = true
-	)
-	private static void serialization_hooks$fromJsonArray(JsonElement json, CallbackInfoReturnable<Ingredient> cir) {
-		JsonArray array = json.getAsJsonArray();
-		for (IngredientDeserializer serializer : IngredientDeserializer.REGISTRY) {
-			Ingredient deserialized = serializer.fromJsonArray(array);
+		if (json == null || json.isJsonNull()) {
+			throw new JsonSyntaxException("Item cannot be null");
+		}
+		for (IngredientDeserializer deserializer : IngredientDeserializer.REGISTRY) {
+			Ingredient deserialized = deserializer.fromJson(json);
 			if (deserialized == null)
 				continue;
 			cir.setReturnValue(deserialized);
